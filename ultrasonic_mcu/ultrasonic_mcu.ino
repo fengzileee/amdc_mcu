a8b2a7881e4d705cef2c84a21796be5e5060e341
randolph <wwk.randolph@gmail.com>
2017-07-20 19:15:58 +0800

    Correct bad handling of error; Implement lock
    1. Publish capped distance for error case 1 and 3 since they are likely
       due to sensor detecting the range to be greater than the capped
       distance.
    2. Make get_proximity function to be atomic via a lock. This is in case
       the I2C callback interrupts in the middle of some stuffs.

--- ultrasonic_mcu/ultrasonic_mcu.ino
+++ ultrasonic_mcu/ultrasonic_mcu.ino
@@ -2,55 +2,95 @@
 // http://www.pcserviceselectronics.co.uk/arduino/Ultrasonic/betterecho.php
 #include <Wire.h>
 
-#define handle_error(N) \
-do {                    \
-    error_code = (N);   \
-    return;             \
-} while (0);
-
 #define ECHO_PIN 5
 #define TRIGGER_PIN 4
 
 // ratio based on speed of sound
 const unsigned long TIME_DISTANCE_RATIO = 58;
-// default distance (in case of error)
-const uint16_t DEFAULT_DIST = 500;
 
 // max time to wait for echo pin to become high
 const unsigned long START_ECHO_TIMEOUT = 1000;
-// max distance that we care about (longer distance means we need to
-// wait longer for sensor to reply)
-const unsigned long MAX_DISTANCE = 500;
+// MAX_DISTANCE corresponse to how long we are willing to wait for the
+// sensor to read the bounced back wave. If we set this too low, then
+// the echo pin may remain high in the next interval (unless we set a
+// relatively large interval like 200ms)
+const unsigned long MAX_DISTANCE = 5000;
 const unsigned long MAX_ECHO_LEN = TIME_DISTANCE_RATIO * MAX_DISTANCE;
+const uint16_t DISTANCE_CAP = 600;
 
-// distance in cm, default to 0
-volatile uint16_t dist = DEFAULT_DIST;
+// distance in cm, default to 600
+volatile uint16_t dist = DISTANCE_CAP;
 volatile uint8_t error_code = 0;
+
+volatile uint16_t old_dist = DISTANCE_CAP;
+volatile uint8_t old_error_code = 0;
 
 // used for creating delay for each reading interval
 unsigned long time, next_time;
 // time between each sensor reading (in ms)
-const unsigned long INTERVAL = 100;
+// We use 200ms because experiment shows that the echo pin can be high
+// for roughly 190ms.
+const unsigned long INTERVAL = 200;
+
+// Lock to ensure atomicity of get_proximity (since I2C callback is
+// handled by interrupt)
+volatile int lock = 0;
+#define LOCKED   1
+#define RELEASED 0
 
 #ifndef I2C_ADDRESS
 #define I2C_ADDRESS 2
 #endif
 
+// error handling
+#define  NO_ERROR             0
+#define  ECHO_HIGH_B4_SET     1
+#define  ECHO_REMAINS_LOW     2
+#define  ECHO_EXCEED_MAX      3
+#define  END_LESS_THAN_START  4
+
+#define handle_error_default(N) \
+do {                            \
+    error_code = (N);           \
+    dist = DISTANCE_CAP;        \
+    lock = RELEASED;            \
+    return;                     \
+} while (0);
+
+#define handle_error_prev(N)    \
+do {                            \
+    error_code = (N);           \
+    lock = RELEASED;            \
+    return;                     \
+} while (0);
+
 void callback()
 {
     unsigned char *data = (unsigned char *) &dist;
+    uint8_t *error = (uint8_t *) &error_code;
+
+    if (lock == LOCKED)
+    {
+        data = (unsigned char *) &old_dist;
+        error = (uint8_t *) &old_error_code;
+    }
+
     Wire.write(data[0]);
     Wire.write(data[1]);
-    Wire.write(error_code);
+    Wire.write(*error);
 }
 
 void get_proximity()
 {
     unsigned long start, end, duration;
 
+    lock = LOCKED;
+
     // error if echo pin is high before we even set it
+    // Possible reason is that the echo pin is still high from previous
+    // reading, suggesting that the range is large.
     if (digitalRead(ECHO_PIN) == HIGH)
-        handle_error(1);
+        handle_error_default(ECHO_HIGH_B4_SET);
 
     start = micros();
 
@@ -63,23 +103,28 @@ void get_proximity()
     end = start + START_ECHO_TIMEOUT;
     while (digitalRead(ECHO_PIN) == LOW)
         if (micros() > end)
-            handle_error(2);
+            handle_error_prev(ECHO_REMAINS_LOW);
 
     // measure length of echo (capped to MAX_ECHO_LEN)
+    // Length greater than MAX_ECHO_LEN means range is large.
     start = micros();
     end = start + MAX_ECHO_LEN;
     while (digitalRead(ECHO_PIN) == HIGH)
         if (micros() > end)
-            handle_error(3);
+            handle_error_default(ECHO_EXCEED_MAX);
     end = micros();
 
     // set distance to default in case end < start
+    // Possible due to integer overflow.
     duration = end - start;
     if (end < start)
-        handle_error(4);
+        handle_error_prev(END_LESS_THAN_START);
 
-    error_code = 0;
+    error_code = NO_ERROR;
     dist = duration/TIME_DISTANCE_RATIO;
+    dist = dist <= DISTANCE_CAP ? dist : DISTANCE_CAP;
+
+    lock = RELEASED;
 }
 
 void setup()
@@ -89,7 +134,6 @@ void setup()
     digitalWrite(TRIGGER_PIN, LOW);
     pinMode(ECHO_PIN, INPUT);
 
-    // join I2C bus as device #8
     Wire.begin(I2C_ADDRESS);
     Wire.onRequest(callback);
 }
